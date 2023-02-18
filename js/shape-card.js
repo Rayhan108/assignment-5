d03bd5836b4ea019df048bbd06a78f7bba0077e0
Rayhan Shorker <raihanshorker56@gmail.com>
2023-02-18 15:56:29 +0600

    added validation for traingle and rectangle shape

--- js/shape-card.js
+++ js/shape-card.js
@@ -1,8 +1,23 @@
 // traingle area---------------------------
 document.getElementById('triangle-area').addEventListener('click',function(){
     const traingleBase =getElementValueById('base');
+   
+    if(isNaN(traingleBase)){
+        alert('please provide a valid number');
+    }else if(traingleBase<0){
+        alert('please provide a positive number');
+        return;}
     const traingleHeight = getElementValueById('height');
-    const traingleArea = 0.5*traingleBase*traingleHeight;
+    
+    if(isNaN(traingleHeight)){
+        alert('please provide a valid number');
+    }else if(traingleHeight<0){
+        alert('please provide a positive number');
+        return;}
+    const traingleAreaValue = 0.5*traingleBase*traingleHeight;
+    const traingleAreaValueString = traingleAreaValue.toFixed(2);
+    const traingleArea = traingleAreaValueString;
+
     
 
     const tableContainer = document.getElementById('table-container');
@@ -19,8 +34,22 @@ document.getElementById('triangle-area').addEventListener('click',function(){
 // rectangle area--------------------------------------------
 document.getElementById('rectangle-area').addEventListener('click',function(){
     const rectangleWidth =getElementValueById('width');
+    if(isNaN(rectangleWidth)){
+        alert('please provide a valid number');
+    }else if(rectangleWidth<0){
+        alert('please provide a positive number');
+        return;}
     const rectangleHeight = getElementValueById('rectangle-height');
-    const rectangleArea =rectangleWidth*rectangleHeight;
+    if(isNaN(rectangleHeight)){
+        alert('please provide a valid number');
+    }else if(rectangleHeight<0){
+        alert('please provide a positive number');
+        return;}
+    const rectangleAreaValue =rectangleWidth*rectangleHeight;
+    const rectangleAreaValueString = rectangleAreaValue.toFixed(2);
+    rectangleArea = rectangleAreaValueString;
+    
+
  
     const tableContainer = document.getElementById('table-container');
     const tr = document.createElement('tr');
@@ -38,7 +67,10 @@ document.getElementById('rectangle-area').addEventListener('click',function(){
 document.getElementById('parallelogram-area').addEventListener('click',function(){
     const parallelogramWidth =getElementValueById('parallelogram-base');
     const parallelogramHeight = getElementValueById('parallelogram-height');
-    const parallelogramArea =parallelogramWidth*parallelogramHeight;
+    const parallelogramalculateValue =parallelogramWidth*parallelogramHeight;
+    const parallelogramAreaValueString = parallelogramalculateValue.toFixed(2);
+    const parallelogramArea = parallelogramAreaValueString;
+
  
     const tableContainer = document.getElementById('table-container');
     const tr = document.createElement('tr');
@@ -55,14 +87,18 @@ document.getElementById('parallelogram-area').addEventListener('click',function(
 document.getElementById('rhombus-area').addEventListener('click',function(){
     const rhombusArm1 =getElementValueById('rhombus-d1');
     const rhombusArm2 = getElementValueById('rhombus-d2');
-    const rhombusArea =0.5*rhombusArm1*rhombusArm2;
+    const rhombosCalculateValue =0.5*rhombusArm1*rhombusArm2;
+   
+    const rhombosAreaValueString = rhombosCalculateValue.toFixed(2);
+    const rhombosArea = rhombosAreaValueString;
+   
  
     const tableContainer = document.getElementById('table-container');
     const tr = document.createElement('tr');
     tr.innerHTML=`
     <td style="padding=2px">${4}.</td>
     <td style="mr-2px:">rhombus</td> 
-    <td style="margin-right:2px">${rhombusArea} cm<sup>2</sup></td> 
+    <td style="margin-right:2px">${rhombosArea} cm<sup>2</sup></td> 
     <td style="background-color: #1090D8;padding:2px">convert to m<sup>2</sup></td>
     `
       tableContainer.appendChild(tr);
@@ -72,14 +108,18 @@ document.getElementById('rhombus-area').addEventListener('click',function(){
 document.getElementById('pentagon-area').addEventListener('click',function(){
     const pentagonPerimeter =getElementValueById('pentagon-perameter');
     const pentagonBase = getElementValueById('pentagon-base');
-    const pentagonArea =0.5*pentagonPerimeter*pentagonBase;
+    const pentagonCalculateValue =0.5*pentagonPerimeter*pentagonBase;
+    
+    const pantagonAreaString = pentagonCalculateValue.toFixed(2);
+    const pantagonArea = pantagonAreaString;
+   
  
     const tableContainer = document.getElementById('table-container');
     const tr = document.createElement('tr');
     tr.innerHTML=`
     <td style="padding=2px">${5}.</td>
     <td style="mr-2px:">pentagon</td> 
-    <td style="margin-right:2px">${pentagonArea} cm<sup>2</sup></td> 
+    <td style="margin-right:2px">${pantagonArea} cm<sup>2</sup></td> 
     <td style="background-color: #1090D8;padding:2px">convert to m<sup>2</sup></td>
     `
       tableContainer.appendChild(tr);
@@ -89,12 +129,17 @@ document.getElementById('pentagon-area').addEventListener('click',function(){
 document.getElementById('ellipse-area').addEventListener('click',function(){
     const ellipseValueA =getElementValueById('ellipse-value1');
     const ellipseValueB = getElementValueById('ellipse-value2');
-    const ellipseArea =3.1416*ellipseValueA*ellipseValueB;
+    const ellipseCalculateValue =3.1416*ellipseValueA*ellipseValueB;
+    const ellipseAreaString = ellipseCalculateValue.toFixed(2);
+    const ellipseArea = ellipseAreaString;
+
+   
+
  
     const tableContainer = document.getElementById('table-container');
     const tr = document.createElement('tr');
     tr.innerHTML=`
-    <td style="padding=2px">${5}.</td>
+    <td style="padding=2px">${6}.</td>
     <td style="mr-2px:">ellipse</td> 
     <td style="margin-right:2px">${ellipseArea} cm<sup>2</sup></td> 
     <td style="background-color: #1090D8;padding:2px">convert to m<sup>2</sup></td>
--- js/utilities.js
+++ js/utilities.js
@@ -1,7 +1,13 @@
 function getElementValueById(elementId){
     const value = document.getElementById(elementId).value;
-    shapeValue =parseFloat(value);
+    shapeValue =Number(value);
     return shapeValue;
 }
     
-
+// function validationById(elementId){
+//     if(isNaN(elementId)){
+//         alert('please provide a valid number');
+//     }else if(elementId<0){
+//         alert('please provide a positive number');
+//         return;}
+//     }
